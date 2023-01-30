b03e1083c4bb871e2e6bd915a43da08060c7a54a
ShahandFahad <shahandfahadmad@gmail.com>
2023-01-30 23:13:57 +0500

    Adding Final Project Week-5

---
+++ Programming-with-JavaScript/Week-5/finalProject.js
@@ -0,0 +1,62 @@
+// Given variables
+const dishData = [
+    {
+        name: "Italian pasta",
+        price: 9.55
+    },
+    {
+        name: "Rice with veggies",
+        price: 8.65
+    },
+    {
+        name: "Chicken with potatoes",
+        price: 15.55
+    },
+    {
+        name: "Vegetarian Pizza",
+        price: 6.45
+    },
+]
+const tax = 1.20;
+
+// Implement getPrices()
+function getPrices(taxBoolean) {
+    
+    for (object of dishData){
+        let finalPrice;
+
+        if(taxBoolean === true){
+            finalPrice = object['price'] * tax;
+        } else if (taxBoolean === false){
+            finalPrice = object['price'];
+        } else {
+            console.log("You need to pass a boolean to the getPrices call!");
+            return;
+        }
+        
+        console.log(`Dish: ${object['name']} Price: $${finalPrice}`);
+    }
+
+}
+
+// Implement getDiscount()
+function getDiscount( taxBoolean, guests) {
+    getPrices(taxBoolean);
+
+    if(typeof guests === "number" && guests > 0 && guests < 30){
+        let discount = 0;
+
+        if(guests < 5){
+            discount = 5;
+        } else if(guests >= 5){
+            discount = 10;
+        }
+        console.log('Discount is: $' + discount);
+    } else {
+            console.log('The second argument must be a number between 0 and 30');
+    }
+}
+
+// Call getDiscount()
+getDiscount(true, 2)
+getDiscount(false, 10)

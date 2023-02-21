afe1627a0d662981e8f88fc8187a564f4786de00
ShahandFahad <shahandfahadmad@gmail.com>
2023-02-21 21:28:06 +0500

    Adding Week-1 Work

---
+++ React-Basics/Week-1/PassingProps/src/App.js
@@ -0,0 +1,14 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName ="Bob" />
+            <Heading firstName ="Shob" />
+            <Heading firstName ="Lob" />
+            <Heading firstName ="Mob" />
+        </div>
+    );
+};
+
+export default App;
